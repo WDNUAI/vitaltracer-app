902c396801474fe8b988fc0de7147218b474d5f5
NoahBilmer <noahbilmer@gmail.com>
2024-07-03 20:37:19 -0400

    Added bluetooth.dart to the repo. Handles BLE bluetooth scanning

---
+++ lib/services/bluetooth.dart
@@ -0,0 +1,44 @@
+import 'dart:developer';
+import 'package:flutter/material.dart';
+import 'dart:async';
+import 'package:flutter_blue_plus/flutter_blue_plus.dart';
+import '../screens/bluetooth-connections-screen.dart';
+
+/// Searches for nearby BLE bluetooth devices.
+/// Uses State<BluetoothConnectionsScreen> [screen] to tell the page to update
+/// it's screen every time we find a new device while scanning.
+Future<List<BluetoothDevice>> searchBle(State<BluetoothConnectionsScreen> screen) async {
+  List<BluetoothDevice> devices = [];
+  // Check if bluetooth is supported on this device.
+  if (await FlutterBluePlus.isSupported == false) {
+    log("Bluetooth not supported by this device");
+    screen.setState(() {});
+    return Future<List<BluetoothDevice>>.error(Exception("Bluetooth not supported"));
+  }
+  // Request to turn on bluetooth. (if it's already on nothing will happen)
+  FlutterBluePlus.turnOn();
+
+  if (!FlutterBluePlus.isScanningNow) {
+    // For now, we don't provide any search criteria and
+    // just search for any nearby BLE devices to test.
+    await FlutterBluePlus.startScan(
+        timeout: Duration(seconds: 15));
+    // withNames:["TBD_NAME_OF_VT"], Eventually our search will be limited to VT devices.
+  }
+  // Listen to our scan results
+    FlutterBluePlus.scanResults.listen((results) {
+      for (ScanResult result in results) {
+        if (!devices.contains(result.device)) {
+          // Make sure our device is not null and for now ignore any devices
+          // without a proper platform name.
+          if (result.device.platformName != null && result.device.platformName.length > 1) {
+            devices.add(result.device);
+            // Call setState every time we add a new device to the list.
+            screen.setState(() {});
+          }
+        }
+      }
+    });
+
+  return devices;
+}
