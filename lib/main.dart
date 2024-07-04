5cb4b045c2fdd095c8c66778739b44b9ec7a5bed
Grant Dioguardi <diog0010@algonquinlive.com>
2024-07-04 01:46:35 -0400

    Add temporary sign up feature with Firebase

--- lib/main.dart
+++ lib/main.dart
@@ -8,9 +8,11 @@ import 'firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
+
   runApp(ChangeNotifierProvider(
     create: (context) => UserInfoModel(),
     child: const MyApp(),
