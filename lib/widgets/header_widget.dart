b02433e8a66295ac244220dcd0aca567f6d4238c
Grant Dioguardi <diog0010@algonquinlive.com>
2024-08-01 04:05:14 -0400

    Add YesNoField widget

--- lib/widgets/header_widget.dart
+++ lib/widgets/header_widget.dart
@@ -1,5 +1,27 @@
 import 'package:flutter/material.dart';
 
+class H4 extends StatelessWidget {
+  final String text;
+
+  const H4({
+    super.key,
+    required this.text,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+    final style = theme.textTheme.headlineSmall!.copyWith(
+      color: theme.colorScheme.outline,
+    );
+
+    return Text(
+      text,
+      style: style,
+    );
+  }
+}
+
 class H3 extends StatelessWidget {
   final String text;
 
