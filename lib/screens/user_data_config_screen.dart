c1247f6fc511a9cea9f1fa1d60ff7f9859b2deda
Grant Dioguardi <diog0010@algonquinlive.com>
2024-07-31 23:36:56 -0400

    Complete welcome screen

--- lib/screens/user_data_config_screen.dart
+++ lib/screens/user_data_config_screen.dart
@@ -3,56 +3,176 @@ import 'package:vitaltracer_app/screens/home_screen.dart';
 import 'package:vitaltracer_app/widgets/header_widget.dart';
 import 'package:vitaltracer_app/widgets/date_selector_widget.dart';
 
-class UserDataConfigScreen extends StatelessWidget {
+class UserDataConfigScreen extends StatefulWidget {
   const UserDataConfigScreen({super.key});
+
+  @override
+  State<UserDataConfigScreen> createState() => _UserDataConfigScreenState();
+}
+
+class _UserDataConfigScreenState extends State<UserDataConfigScreen> {
+  final _formKey = GlobalKey<FormState>();
+
+  final double _topPadding = 20;
+  final double _bottomPadding = 20;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(
-        child: Padding(
-          padding: const EdgeInsets.fromLTRB(20, 40, 20, 40),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              const H1(text: "Welcome"),
-              const SizedBox(height: 10),
-              const H2(text: "Please provide the following information."),
-              const SizedBox(height: 20),
-              const H3(text: "Date of Birth"),
-              const SizedBox(height: 20),
-              const DateSelector(),
-              const SizedBox(height: 20),
-              const H3(text: "Gender"),
-              const SizedBox(height: 20),
-              const Row(
-                children: [
-                  Expanded(
-                    child: ListTile(title: Text("Male")),
-                  ),
-                  Expanded(
-                    child: ListTile(title: Text("Female")),
-                  ),
-                  Expanded(
-                      child: ListTile(title: Text("Prefer not to answer"))),
-                ],
-              ),
-              Expanded(
-                child: Align(
-                  alignment: Alignment.bottomCenter,
-                  child: ElevatedButton(
-                    onPressed: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                            builder: (context) => const HomeScreen()),
+      body: Form(
+        key: _formKey,
+        child: SafeArea(
+          child: Padding(
+            padding: EdgeInsets.fromLTRB(20, _topPadding, 20, _bottomPadding),
+            child: SingleChildScrollView(
+              child: SizedBox(
+                height: MediaQuery.sizeOf(context).height -
+                    MediaQuery.paddingOf(context).vertical -
+                    _topPadding -
+                    _bottomPadding,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    const H1(text: "Welcome"),
+                    const SizedBox(height: 10),
+                    const H2(text: "Please provide the following information."),
+                    const SizedBox(height: 20),
+                    const H3(text: "Date of Birth"),
+                    const SizedBox(height: 20),
+                    const DateSelector(),
+                    const SizedBox(height: 20),
+                    const H3(text: "Gender"),
+                    const SizedBox(height: 20),
+                    FormField(builder: (field) {
+                      int gender = 0;
+
+                      return Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Radio(
+                            value: 1,
+                            groupValue: gender,
+                            onChanged: (value) {
+                              field.setState(() {
+                                gender = value!;
+                              });
+                            },
+                            visualDensity: const VisualDensity(
+                              horizontal: VisualDensity.minimumDensity,
+                              vertical: VisualDensity.minimumDensity,
+                            ),
+                          ),
+                          const Text("Male"),
+                          Radio(
+                            value: 2,
+                            groupValue: gender,
+                            onChanged: (value) {
+                              field.setState(() {
+                                gender = value!;
+                              });
+                            },
+                            visualDensity: const VisualDensity(
+                              horizontal: VisualDensity.minimumDensity,
+                              vertical: VisualDensity.minimumDensity,
+                            ),
+                          ),
+                          const Text("Female"),
+                          Radio(
+                            value: 3,
+                            groupValue: gender,
+                            onChanged: (value) {
+                              field.setState(() {
+                                gender = value!;
+                              });
+                            },
+                            visualDensity: const VisualDensity(
+                              horizontal: VisualDensity.minimumDensity,
+                              vertical: VisualDensity.minimumDensity,
+                            ),
+                          ),
+                          const Text("Prefer not to answer"),
+                        ],
                       );
-                    },
-                    child: const Text("Confirm"),
-                  ),
+                    }),
+                    const SizedBox(height: 20),
+                    TextFormField(
+                      decoration: InputDecoration(
+                        border: const OutlineInputBorder(),
+                        fillColor:
+                            Theme.of(context).colorScheme.primaryContainer,
+                        hintText: "Your Height",
+                        suffixText: "cm",
+                      ),
+                      validator: (String? value) {
+                        if (value!.isEmpty) {
+                          return "Please specify your height";
+                        }
+
+                        int? height = int.tryParse(value!);
+                        if (height == null) {
+                          return "Height must be a number";
+                        } else if (height < 40) {
+                          return "Height cannot be less than 40cm";
+                        } else if (height > 300) {
+                          return "Height cannot be higher than 300cm";
+                        }
+
+                        return null;
+                      },
+                    ),
+                    const SizedBox(height: 10),
+                    TextFormField(
+                      decoration: InputDecoration(
+                        border: const OutlineInputBorder(),
+                        fillColor:
+                            Theme.of(context).colorScheme.primaryContainer,
+                        hintText: "Your Weight",
+                        suffixText: "kg",
+                      ),
+                      validator: (String? value) {
+                        if (value!.isEmpty) {
+                          return "Please specify your height";
+                        }
+
+                        int? weight = int.tryParse(value!);
+                        if (weight == null) {
+                          return "Weight must be a number";
+                        } else if (weight < 10) {
+                          return "Weight cannot be less than 10kg";
+                        } else if (weight > 500) {
+                          return "Weight cannot be higher than 500kg";
+                        }
+
+                        return null;
+                      },
+                    ),
+                    Expanded(
+                      child: Align(
+                        alignment: Alignment.bottomCenter,
+                        child: FilledButton(
+                          style: FilledButton.styleFrom(
+                            minimumSize: const Size(250, 40),
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(5),
+                            ),
+                          ),
+                          child: const Text("NEXT"),
+                          onPressed: () {
+                            if (_formKey.currentState!.validate()) {
+                              Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                    builder: (context) => const HomeScreen()),
+                              );
+                            }
+                          },
+                        ),
+                      ),
+                    ),
+                  ],
                 ),
               ),
-            ],
+            ),
           ),
         ),
       ),
